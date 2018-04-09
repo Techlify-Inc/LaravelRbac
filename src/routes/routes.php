c34b9c4d1f8b7d4ced4b5969f4976884fdf289f5
Techlify Inc <info@techlify.com>
2018-04-09 10:51:07 -0400

    Setup routes for API Resources provided

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,1 +1,10 @@
 <?php
+
+Route::group(['prefix' => 'api', 'middleware' => 'api'], function()
+{
+    Route::resource("roles", "\TechlifyInc\LaravelRbac\Controllers\RoleController");
+    Route::patch("roles/{role}/permissions/{permission}/add", "\TechlifyInc\LaravelRbac\Controllers\RoleController@addPermission");
+    Route::patch("roles/{role}/permissions/{permission}/remove", "\TechlifyInc\LaravelRbac\Controllers\RoleController@removePermission");
+
+    Route::resource("permissions", "\TechlifyInc\LaravelRbac\Controllers\PermissionController");
+});
